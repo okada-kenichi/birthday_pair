e55c7c98b83c66a5389265ce203b2e9e0b59905c
akihiro_hayashida <yaurevoci@gmail.com>
2021-07-17 01:23:18 +0900

    TOPIC: VITE JS構成

---
+++ src/components/forms.vue
@@ -0,0 +1,26 @@
+<template>
+  <form>
+    <select id="myNum">
+      <option v-for="num in classNumber" :key="num" :value="num">
+        {{ num }}人の教室▼
+      </option>
+    </select>
+  </form>
+</template>
+
+<script>
+import { defineComponent, computed } from "vue";
+
+export default defineComponent({
+  setup() {
+    const classNumber = computed(() => {
+      return [...Array(366)].map((_, i) => i);
+    });
+    return {
+      classNumber,
+    };
+  },
+});
+</script>
+<style scoped>
+</style>
